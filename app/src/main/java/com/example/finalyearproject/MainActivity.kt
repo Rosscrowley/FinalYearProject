847b3a3b25e446ea674da9d18567245e5e46c856
Rosscrowley <91560583+Rosscrowley@users.noreply.github.com>
2023-10-19 16:21:08 +0100

    First commit - Sign up and log in

---
+++ app/src/main/java/com/example/finalyearproject/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.finalyearproject
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    private val RQ_SPEECH_REC = 102
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button2)
+
+        btn.setOnClickListener {
+
+            askSpeechInput()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val textV = findViewById<TextView>(R.id.textView2)
+
+        if (requestCode == RQ_SPEECH_REC && resultCode == Activity.RESULT_OK){
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            textV.text = result?.get(0).toString()
+        }
+    }
+
+    private fun askSpeechInput() {
+
+        if(!SpeechRecognizer.isRecognitionAvailable(this)){
+            Toast.makeText(this,"Speech recognition is not available", Toast.LENGTH_SHORT).show()
+        }else{
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something!")
+            startActivityForResult(i,RQ_SPEECH_REC)
+        }
+
+    }
+}
